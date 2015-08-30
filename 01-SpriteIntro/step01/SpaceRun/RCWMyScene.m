36c697140b0e2fcc6c1ee9876f9960eb36635589
iphoneReadingme <iphoneReadingme@users.noreply.github.com>
2015-08-30 16:01:50 +0800

    Build iOS Games with Sprite Kit: Unleash Your Imagination in Two Dimensions2014-09

---
+++ 01-SpriteIntro/step01/SpaceRun/RCWMyScene.m
@@ -0,0 +1,26 @@
+/***
+ * Excerpted from "Build iOS Games with Sprite Kit",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/pssprite for more book information.
+***/
+#import "RCWMyScene.h"
+
+@implementation RCWMyScene
+
+- (id)initWithSize:(CGSize)size
+{
+    if (self = [super initWithSize:size]) {
+        self.backgroundColor = [SKColor blackColor];
+
+        NSString *name = @"Spaceship.png";
+        SKSpriteNode *ship = [SKSpriteNode spriteNodeWithImageNamed:name];
+        ship.position = CGPointMake(size.width/2, size.height/2);
+        [self addChild:ship];
+    }
+    return self;
+}
+
+@end
