6ebaecc2861a5e9e6f2de8108b11b9cdaf43e80d
iphoneReadingme <iphoneReadingme@users.noreply.github.com>
2015-08-31 10:09:02 +0800

    Demo:

---
+++ 01-SpriteIntro/step05/SpaceRun/RCWMyScene.m
@@ -0,0 +1,69 @@
+/***
+ * Excerpted from "Build iOS Games with Sprite Kit",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/pssprite for more book information.
+***/
+#import "RCWMyScene.h"
+
+@interface RCWMyScene ()
+@property (nonatomic, weak) UITouch *shipTouch;
+@property (nonatomic) NSTimeInterval lastUpdateTime;
+@end
+
+@implementation RCWMyScene
+
+- (id)initWithSize:(CGSize)size
+{
+    if (self = [super initWithSize:size]) {
+        self.backgroundColor = [SKColor blackColor];
+
+        NSString *name = @"Spaceship.png";
+        SKSpriteNode *ship = [SKSpriteNode spriteNodeWithImageNamed:name];
+        ship.position = CGPointMake(size.width/2, size.height/2);
+        ship.size = CGSizeMake(40, 40);
+        ship.name = @"ship";
+        [self addChild:ship];
+    }
+    return self;
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    self.shipTouch = [touches anyObject];
+}
+
+- (void)update:(NSTimeInterval)currentTime
+{
+    if (self.lastUpdateTime == 0) {
+        self.lastUpdateTime = currentTime;
+    }
+    NSTimeInterval timeDelta = currentTime - self.lastUpdateTime;
+
+    if (self.shipTouch) {
+        [self moveShipTowardPoint:[self.shipTouch locationInNode:self]
+                      byTimeDelta:timeDelta];
+    }
+    self.lastUpdateTime = currentTime;
+}
+
+- (void)moveShipTowardPoint:(CGPoint)point byTimeDelta:(NSTimeInterval)timeDelta
+{
+    CGFloat shipSpeed = 130; // points per second
+    SKNode *ship = [self childNodeWithName:@"ship"];
+    CGFloat distanceLeft = sqrt(pow(ship.position.x - point.x, 2) +
+                                pow(ship.position.y - point.y, 2));
+    if (distanceLeft > 4) {
+        CGFloat distanceToTravel = timeDelta * shipSpeed;
+        CGFloat angle = atan2(point.y - ship.position.y,
+                              point.x - ship.position.x);
+        CGFloat yOffset = distanceToTravel * sin(angle);
+        CGFloat xOffset = distanceToTravel * cos(angle);
+        ship.position = CGPointMake(ship.position.x + xOffset,
+                                    ship.position.y + yOffset);
+    }
+}
+
+@end
---
+++ 03-Particles/step03/SpaceRunTests/SpaceRunTests.m
@@ -0,0 +1,34 @@
+/***
+ * Excerpted from "Build iOS Games with Sprite Kit",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/pssprite for more book information.
+***/
+#import <XCTest/XCTest.h>
+
+@interface SpaceRunTests : XCTestCase
+
+@end
+
+@implementation SpaceRunTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
