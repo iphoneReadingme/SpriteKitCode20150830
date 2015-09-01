e2a63557391e68cc03e762ec45a9ed42f163798d
iphoneReadingme <iphoneReadingme@users.noreply.github.com>
2015-09-01 20:56:36 +0800

    Demo:
    Chapter 2
    shooting at Asteroids with Simple Motion Actions

--- 02-Actions/step01/SpaceRun/RCWMyScene.m
+++ 02-Actions/step01/SpaceRun/RCWMyScene.m
@@ -47,11 +47,6 @@
 		CGPoint touchPoint = [_shipTouch locationInNode:self];
 		
         [self moveShipTowardPoint:touchPoint byTimeDelta:timeDelta];
-
-        if (currentTime - self.lastShotFireTime > 0.5) {
-            [self shoot];
-            self.lastShotFireTime = currentTime;
-        }
     }
 
     self.lastUpdateTime = currentTime;
@@ -81,17 +76,6 @@
 
 - (void)shoot
 {
-    SKNode *ship = [self childNodeWithName:@"ship"];
-
-    SKSpriteNode *photon = [SKSpriteNode spriteNodeWithImageNamed:@"photon"];
-    photon.name = @"photon";
-    photon.position = ship.position;
-    [self addChild:photon];
-
-    SKAction *fly = [SKAction moveByX:0
-                                    y:self.size.height+photon.size.height
-                             duration:0.5];
-    [photon runAction:fly];
 }
 
 @end
