75d42a25703bc091bf8eeae9f0b087c2c6e17b22
iphoneReadingme <iphoneReadingme@users.noreply.github.com>
2015-09-08 07:41:24 +0800

    Demo:
    Chapter 3
    Explosions and Particle Effects
    
    
    1. “Generating a Parallax Field of Stars”

---
+++ 03-Particles/step01/SpaceRun/RCWStarField.m
@@ -0,0 +1,45 @@
+/***
+ * Excerpted from "Build iOS Games with Sprite Kit",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/pssprite for more book information.
+***/
+#import "RCWStarField.h"
+@implementation RCWStarField
+- (instancetype)init
+{
+    if (self = [super init]) {
+        __weak RCWStarField *weakSelf = self;
+        SKAction *update =[SKAction runBlock:^{
+            if (arc4random_uniform(10) < 3) {
+                [weakSelf launchStar];
+            }
+        }];
+        SKAction *delay = [SKAction waitForDuration:0.01];
+        SKAction *updateLoop = [SKAction sequence:@[delay, update]];
+        [self runAction:[SKAction repeatActionForever:updateLoop]];
+    }
+    return self;
+}
+- (void)launchStar
+{
+    CGFloat randX = arc4random_uniform(self.scene.size.width);
+    CGFloat maxY = self.scene.size.height;
+    CGPoint randomStart = CGPointMake(randX, maxY);
+
+    SKSpriteNode *star = [SKSpriteNode spriteNodeWithImageNamed:@"shootingstar"];
+    star.position = randomStart;
+    star.size = CGSizeMake(2, 10);
+    star.alpha = 0.1 + (arc4random_uniform(10) / 10.0f);
+    [self addChild:star];
+
+    CGFloat destY = 0 - self.scene.size.height - star.size.height;
+    CGFloat duration = 0.1 + arc4random_uniform(10) / 10.0f;
+    SKAction *move = [SKAction moveByX:0 y:destY duration:duration];
+    SKAction *remove = [SKAction removeFromParent];
+    [star runAction:[SKAction sequence:@[move, remove]]];
+}
+
+@end
---
+++ 03-Particles/step01/SpaceRun/RCWViewController.m
@@ -0,0 +1,51 @@
+/***
+ * Excerpted from "Build iOS Games with Sprite Kit",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/pssprite for more book information.
+***/
+#import "RCWViewController.h"
+#import "RCWMyScene.h"
+
+@implementation RCWViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Configure the view.
+    SKView * skView = (SKView *)self.view;
+    skView.showsFPS = YES;
+    skView.showsNodeCount = YES;
+    
+    // Create and configure the scene.
+    SKScene * scene = [RCWMyScene sceneWithSize:skView.bounds.size];
+    scene.scaleMode = SKSceneScaleModeAspectFill;
+    
+    // Present the scene.
+    [skView presentScene:scene];
+}
+
+- (BOOL)shouldAutorotate
+{
+    return YES;
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        return UIInterfaceOrientationMaskAllButUpsideDown;
+    } else {
+        return UIInterfaceOrientationMaskAll;
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+@end
